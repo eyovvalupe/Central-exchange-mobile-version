60a3670ce6ceeb12bbe37cbc3da473e7fb0883d0
eyovvalupe <rylantwcn705172@hotmail.com>
2025-02-22 14:18:12 +0800

    lorna上盘相关配置

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
  *  颜色_日期_字母 
  *  wb  白底蓝色
  */
-export const VERSION = 'bg2_0217_a'
+export const VERSION = 'lorna_0222_a'
 
 
 /* 首页模板 */
@@ -12,32 +12,13 @@ export const HOME_MODE = 2
 /* 底部导航模板 */
 export const BOTTOMBAR_MODE = 1
 
-/* 开发环境 */
-// export const BASE_SOCKET = 'http://54.179.115.4:9093'; // ws地址
-// export const BASE_ADDRESS = 'http://54.179.115.4:8000'; // 请求地址 62.112.10.184:8302
-// export const CHAT_WEBSOCKET = 'http://54.179.115.4:9095'; // 客服ws地址
-
-
-/* 内测版本 */
-export const BASE_SOCKET = 'wss://9093.sungame.shop'; // ws地址
-export const BASE_ADDRESS = 'https://api.sungame.shop'; // 请求地址 62.112.10.184:8302
-export const CHAT_WEBSOCKET = 'wss://9095.sungame.shop'; // 客服ws地址
-export const MOBILE_INVITE_URL = 'https://blackgreenh5.sungame.shop/#/'
-export const DESKTOP_INVITE_URL = 'https://blackgreen.sungame.shop/#/'
-
-
-/* 演示版本 */
-// export const BASE_SOCKET = 'wss://9093.sunex.game'; // ws地址
-// export const BASE_ADDRESS = 'https://api.sunex.game'; // 请求地址 62.112.10.184:8302
-// export const CHAT_WEBSOCKET = 'wss://9095.sunex.game'; // 客服ws地址
-// export const MOBILE_INVITE_URL = 'https://h5.sunex.game/#/'
-// export const DESKTOP_INVITE_URL = 'https://www.sunex.game/#/'
-
-// export const MOBILE_INVITE_URL = 'http://localhost:3000/#/'
-// export const DESKTOP_INVITE_URL = 'http://localhost:8080/#/'
-
-// export const UPLOAD_ADDRESS = 'https://api.github.com/repos/watershui1222/image/contents/'; 
-export const UPLOAD_ADDRESS = 'https://api.github.com/repos/eyovvalupe/saveImage/contents/';
-export const UPLOAD_TOKEN = 'Bearer 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';
+/* 接口配置 */
+export const BASE_SOCKET = 'wss://9093.lorna.ink'; // ws地址
+export const BASE_ADDRESS = 'https://api.lorna.ink'; // 请求地址 62.112.10.184:8302
+export const CHAT_WEBSOCKET = 'wss://9095.lorna.ink'; // 客服ws地址
+export const MOBILE_INVITE_URL = 'https://lorna.ink'
+export const DESKTOP_INVITE_URL = 'https://lorna.ink'
+export const UPLOAD_ADDRESS = 'https://api.github.com/repos/lorna883/lorna-ink/contents/';
+export const UPLOAD_TOKEN = 'Bearer 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';
 
 export const default_language = 'en'
