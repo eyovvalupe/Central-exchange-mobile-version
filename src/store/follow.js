d84bbc7ab9ba0a6371c8526f84d06c93481817a8
willam <willam@gmail.com>
2025-03-17 14:23:31 +0800

    feat: 跟单loading

--- src/store/follow.js
+++ src/store/follow.js
@@ -3,10 +3,8 @@ import { _copyList, _copyMyData, _copyMyList } from '@/api/api'
 
 export default {
   state: {
-    followListLoading:false,
-    followList: [], // 跟单列表
-    myCopyLoading:false,
     myCopy: [], // 我的跟单
+    myCopyLoading:false,
     copyItem: JSON.parse(sessionStorage.getItem('copyItem')),
     myCopyData: {},
     copyItemDetail: JSON.parse(sessionStorage.getItem('copyItemDetail'))
@@ -24,36 +22,14 @@ export default {
     setCopyItem(state, data) {
       state.copyItem = data
     },
-    setFollowList(state, data) {
-      state.followList = data || []
-    },
-    setFollowListLoading(state, data) {
-      state.followListLoading = data
-    },
+   
     setMyCopy(state, data) {
       state.myCopy = data || []
     },
   },
   actions: {
-    updateFollowList({ commit }) {
-      // 更新跟单列表
-      return new Promise(resolve => {
-        commit('setFollowListLoading',true)
-        _copyList({ page: 1 })
-          .then(res => {
-            if (res.code == 200 && res.data) {
-              commit('setFollowList', res.data || [])
-              resolve(res.data)
-            } else {
-              resolve(false)
-            }
-          })
-          .catch(() => resolve(false)).finally(()=>{
-            commit('setFollowListLoading', false)
-          })
-      })
-    },
-    updateMyFollowList({ commit }) {
+    
+    updateMyFollowList({ commit,state }) {
       // 更新我的跟单列表
       return new Promise(resolve => {
         commit('setMyCopyLoading', true)
